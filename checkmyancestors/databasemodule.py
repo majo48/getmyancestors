cec0620e02ff3844dd7fee1c72f3941778686088
majo48 <mart.jonasse@gmail.com>
2019-12-23 10:21:23 +0100

    Added insert_person function to SQLite databasemodule.py. Aligned initialization function.

--- checkmyancestors/databasemodule.py
+++ checkmyancestors/databasemodule.py
@@ -27,7 +27,8 @@
 # global imports
 import os
 import sqlite3
-
+from sqlite3.dbapi2 import Connection, Cursor
+from checkmyancestors import app
 
 class Database:
     """
@@ -36,45 +37,76 @@ class Database:
 
     def __init__(self):
         """ initialize the SQLite database"""
+        conn: Connection = self._get_connection()
+        cursor: Cursor = conn.cursor()
+        try:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS persons (
+        	        personid      TEXT NOT NULL,
+	                timestamp	  INTEGER NOT NULL,
+	                referenceid   TEXT NOT NULL,
+	                status_list   TEXT NOT NULL,
+	                generation    INTEGER NOT NULL,
+	                fsperson      TEXT,
+	                name          TEXT,
+	                gender        TEXT,
+	                born          TEXT,
+	                lifespan      TEXT,
+	                fatherid      TEXT,
+	                motherid      TEXT,
+	                relationships TEXT,
+	                last_modified INTEGER,
+	                PRIMARY KEY(personid,timestamp,referenceid)); """)
+            conn.commit()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS sessions (
+	                timestamp     INTEGER NOT NULL,
+	                person_count  INTEGER NOT NULL,
+	                status        TEXT,
+	                change_log    TEXT,
+	                PRIMARY KEY(timestamp)); """)
+            conn.commit()
+            conn.close()
+        #
+        except sqlite3.Error as e:
+            app.write_log("SQLite CREATE TABLE error occurred:", e.args[0])
+
+    def _get_connection(self):
+        """ get SQLite connection object """
         path = os.path.dirname(os.path.realpath(__file__))
-        conn = sqlite3.connect(path + '/' + 'database.db')
-        cursor = conn.cursor()
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS "persons" (
-        	    "personid"      TEXT NOT NULL,
-	            "timestamp"	    INTEGER NOT NULL,
-	            "referenceid"   TEXT NOT NULL,
-	            "status_list"   TEXT NOT NULL,
-	            "generation"    INTEGER NOT NULL,
-	            "fsperson"      TEXT,
-	            "name"          TEXT,
-	            "gender"        TEXT,
-	            "born"          TEXT,
-	            "lifespan"      TEXT,
-	            "fatherid"      TEXT,
-	            "motherid"      TEXT,
-	            "relationships" TEXT,
-	            "last_modified" INTEGER,
-	            PRIMARY KEY("personid","timestamp","referenceid")
-                );
-        """)
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS "sessions" (
-	            "timestamp"    INTEGER NOT NULL,
-	            "person_count"  INTEGER NOT NULL,
-	            "status"       TEXT,
-	            "change_log"    TEXT,
-	            PRIMARY KEY("timestamp")
-                );
-        """)
+        return sqlite3.connect(path + '/' + 'database.db')
+
+    def _insert_person(self, person):
+        """ insert the person object in the SQLite database
+            Args:
+                 person (app.PersonObj): object data downloaded from FamilySearch
+        """
+        conn: Connection = self._get_connection()
+        cursor: Cursor = conn.cursor()
+        sql = """
+            INSERT INTO persons 
+                (personid, timestamp, referenceid, status_list, generation, 
+                 fsperson, name, gender, born, lifespan, 
+                 fatherid, motherid, relationships, last_modified)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        try:
+            cursor.execute( sql,
+                (person.personid, person.timestamp, person.referenceid, person.status_list, person.generation,
+                 person.fsperson, person.name, person.gender, person.born, person.lifespan,
+                 person.fatherid, person.motherid, person.relationships, person.last_modified))
+            conn.commit()
+            conn.close()
+        #
+        except sqlite3.Error as e:
+            app.write_log("SQLite INSERT person error occurred:", e.args[0])
 
     def persist_person(self, person):
-        """ persist person to database
+        """ persist person to database conditionally
             Args:
-                 person (PersonObj): object data downloaded from FamilySearch
+                 person (app.PersonObj): object data downloaded from FamilySearch
         """
-        print("persisted " + person.name + ', ' + person.lifespan + "[" + str(person.generation) + "]")
-        #todo continue here...
+        self._insert_person(person)
 
 
 def main():
